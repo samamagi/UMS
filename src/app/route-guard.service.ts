aee93bb8db1e59db218df741220aab11a00a27e6
samamagi <giancarlo.sparano@gmail.com>
2020-04-13 17:45:11 +0200

    lezione 73

---
+++ src/app/route-guard.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+@Injectable()
+export class RouteGuardService implements CanActivate {
+
+  constructor(private route: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    this.route.navigate(['users']);
+    return false ;
+  }
+}
