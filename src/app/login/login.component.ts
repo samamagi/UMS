61ce9ad270de7eff4c271c7ea2c1e740c9ab0216
samamagi <giancarlo.sparano@gmail.com>
2020-04-13 20:34:41 +0200

    lezione 77

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   signIn(form: NgForm) {
-    alert('valid: ' + form.valid);
-    alert('email: ' + form.value.email);
+    if (!form.valid) {
+      return false;
+    }
+    let result = this.auth.signIn(form.value.email, form.value.password);
+    if (result) {
+      this.router.navigate(['']);
+    }
   }
 }
